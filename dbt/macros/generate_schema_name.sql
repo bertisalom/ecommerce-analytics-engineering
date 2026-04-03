4026b9d688ca31bfa7b034ffdf9a1fea17680dda
Berti <bertisalom@gmail.com>
2026-04-03 11:41:15 +0200

    Configure dbt targets for ephemeral BigQuery CI datasets

--- dbt/macros/generate_schema_name.sql
+++ dbt/macros/generate_schema_name.sql
@@ -1,7 +1,15 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
+    {% set ci_suffix = env_var('DBT_CI_SCHEMA_SUFFIX', '') | trim %}
+
     {% if custom_schema_name is none %}
-        {{ target.schema }}
+        {% set base_schema = target.schema %}
     {% else %}
-        {{ custom_schema_name | trim }}
+        {% set base_schema = custom_schema_name | trim %}
+    {% endif %}
+
+    {% if target.name == 'ci' and ci_suffix %}
+        {{ base_schema }}_ci_{{ ci_suffix }}
+    {% else %}
+        {{ base_schema }}
     {% endif %}
 {% endmacro %}
